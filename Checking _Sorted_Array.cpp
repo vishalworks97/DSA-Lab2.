5350acd7b3b7921e2a27ba21f033ebeadf2b394e
Vishal Kumar <vishalworks97@gmail.com>
2026-09-18 06:59:25 -0700

    Implement isSorted function
    Added isSorted function to check if an array is sorted.

--- Checking _Sorted_Array.cpp
+++ Checking _Sorted_Array.cpp
@@ -98,3 +98,14 @@ int main()
 
     return 0;
 }
+
+bool isSorted(const int* arr, const int size)
+{ for (int i = 0; i < size - 1; i++){
+     if (arr[i] > arr[i + 1])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
